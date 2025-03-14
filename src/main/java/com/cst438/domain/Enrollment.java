ad275f977b77e3d72064e9aab5674654c00ee17c
Henry Garkanian <hgarkanian@csumb.edu>
2025-03-13 20:45:48 -0700

    Commit 1: Inserted relationships and getters and setters within Enrollment class.

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -2,6 +2,8 @@ package com.cst438.domain;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 public class Enrollment {
     @Id
@@ -11,7 +13,43 @@ public class Enrollment {
 	
 	// TODO complete this class
     // add additional attribute for grade
+    int grade;
     // create relationship between enrollment and user entities
+    @OneToMany(mappedBy = "enrollment")
+    List<User> users;
     // create relationship between enrollment and section entities
+    @OneToMany(mappedBy = "enrollment")
+    List<Section> sections;
     // add getter/setter methods
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
 }
