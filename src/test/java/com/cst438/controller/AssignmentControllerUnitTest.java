b75819ede3c0ab5a44ac9d8a620c8b3e96a18f8c
suvorov-csumb <asuvorov@csumb.edu>
2025-03-30 13:32:00 -0500

    -Added testing for when an instructor attempts to grade an assignment, but the assignment id is invalid and instructor entering a grade.

--- src/test/java/com/cst438/controller/AssignmentControllerUnitTest.java
+++ src/test/java/com/cst438/controller/AssignmentControllerUnitTest.java
@@ -3,7 +3,10 @@ package com.cst438.controller;
 
 import com.cst438.domain.Assignment;
 import com.cst438.domain.AssignmentRepository;
+import com.cst438.domain.Grade;
+import com.cst438.domain.GradeRepository;
 import com.cst438.dto.AssignmentDTO;
+import com.cst438.dto.GradeDTO;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.junit.MockitoJUnit;
@@ -29,6 +32,9 @@ public class AssignmentControllerUnitTest {
 
     @Autowired
     AssignmentRepository assignmentRepository;
+
+    @Autowired
+    GradeRepository gradeRepository;
 
     @Test
     public void addAssignment() throws Exception {
@@ -67,5 +73,100 @@ public class AssignmentControllerUnitTest {
         assertNull(assignment);
 
     }
+
+    @Test
+    public void addAssignmentGrade() throws Exception {
+        MockHttpServletResponse response;
+
+        AssignmentDTO assignmentDTO = new AssignmentDTO(
+                0,
+                "Collect samples from Miller's planet",
+                "2025-03-31",
+                "cst363",
+                9,
+                8
+        );
+
+        response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/assignments")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(assignmentDTO)))
+                        .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+        AssignmentDTO assignmentResult = fromJsonString(response.getContentAsString(), AssignmentDTO.class);
+
+        GradeDTO gradeDTO = new GradeDTO(
+                1,
+                "John Smith",
+                "john.smith@csumb.edu",
+                assignmentResult.title(),
+                assignmentResult.courseId(),
+                assignmentResult.secId(),
+                95
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/grades")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(gradeDTO)))
+                .andReturn().getResponse();
+
+        response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/assignments/"+assignmentResult.id()+"/grades")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(gradeDTO)))
+                .andReturn().getResponse();
+
+
+        assertEquals(200, response.getStatus());
+        GradeDTO result = fromJsonString(response.getContentAsString(), GradeDTO.class);
+        Grade grade = gradeRepository.findById(result.gradeId()).orElse(null);
+        assertNotNull(grade);
+        assertEquals(1, result.gradeId());
+        assertEquals("John Smith", result.studentName());
+        assertEquals("john.smith@csumb.edu", result.studentEmail());
+        assertEquals("Collect samples from Miller's planet", result.assignmentTitle());
+        assertEquals(1, result.gradeId());
+        assertEquals(9, result.sectionId());
+        assertEquals(95, result.score());
+
+        response = mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/assignments/"+assignmentResult.id()))
+                .andReturn()
+                .getResponse();
+        assertEquals(200, response.getStatus());
+        Assignment assignment = assignmentRepository.findById(assignmentResult.id()).orElse(null);
+        assertNull(assignment);
+    }
+
+    @Test
+    public void addGradeToInvalidAssignment() throws Exception {
+        MockHttpServletResponse response;
+
+        int invalidAssignmentId = 9999; //assignment ID that does not exist
+        GradeDTO gradeDTO = new GradeDTO(
+                1,
+                "John Smith",
+                "john.smith@csumb.edu",
+                "Collect samples from Miller's planet",
+                "cst363",
+                9,
+                95
+        );
+
+        response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/assignments/" + invalidAssignmentId + "/grades")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(gradeDTO)))
+                .andReturn().getResponse();
+
+        assertEquals(404, response.getStatus());
+    }
+
 
 }
