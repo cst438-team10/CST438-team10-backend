8accea01c469d9cad2f9affe077688da041789d3
Henry Garkanian <hgarkanian@csumb.edu>
2025-03-31 14:50:06 -0700

    Finished enrollIntoSectionTest.

--- src/test/java/com/cst438/controller/EnrollmentControllerUnitTest.java
+++ src/test/java/com/cst438/controller/EnrollmentControllerUnitTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.cst438.test.utils.TestUtils.fromJsonString;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Date;
@@ -31,5 +34,26 @@ public class EnrollmentControllerUnitTest {
                 .andReturn().getResponse();
         assertEquals(400, response.getStatus());
         assertEquals("enrollment closed", response.getErrorMessage());
+    }
+    @Test
+    public void enrollIntoSectionTest() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/enrollments/sections/{sectionNo}?studentId=3", "9")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();;
+        assertEquals(200, response.getStatus());
+        EnrollmentDTO result = fromJsonString(response.getContentAsString(), EnrollmentDTO.class);
+        assertNotEquals(0, result.sectionNo());
+
+        int enrollmentId = result.enrollmentId();
+        response = mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/enrollments/{enrollmentNo}", enrollmentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+        assertEquals(200, response.getStatus());
     }
 }
