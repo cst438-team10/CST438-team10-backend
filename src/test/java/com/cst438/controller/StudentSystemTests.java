ad4bba1100eb119f0b1f5f190c30700f3297c3fa
Polina <zhperon@gmail.com>
2025-04-01 15:37:34 -0700

    Broke the System tests down by the role in App.js

---
+++ src/test/java/com/cst438/controller/StudentSystemTests.java
@@ -0,0 +1,106 @@
+package com.cst438.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class StudentSystemTests {
+    static String osName = System.getProperty("os.name").toLowerCase();
+    Keys myKey = osName.contains("windows") || osName.contains("linux")?Keys.CONTROL:Keys.COMMAND;
+
+    public static final String CHROME_DRIVER_FILE_LOCATION = osName.contains("windows") || osName.contains("linux")?
+            "C:\\chromedriver-win64\\chromedriver.exe": "/Users/henry/chromedriver-mac-x64/chromedriver";
+
+    public static final String URL = "http://localhost:3000";
+
+    public static final int SLEEP_DURATION = 1000; // 1 second.
+
+    // these tests assumes that test data does NOT contain any
+    // sections for course cst499 in 2024 Spring term.
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUpDriver() throws Exception {
+
+        // set properties required by Chrome Driver
+        System.setProperty(
+                "webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+
+        // start the driver
+        driver = new ChromeDriver(ops);
+
+        driver.get(URL);
+        // must have a short wait to allow time for the page to download
+        Thread.sleep(SLEEP_DURATION);
+
+    }
+
+    @AfterEach
+    public void terminateDriver() {
+        if (driver != null) {
+            // quit driver
+            driver.close();
+            driver.quit();
+            driver = null;
+        }
+    }
+    @Test
+    public void systemTestEnroll () throws Exception {
+        String title = "Software Design";
+        String section = "6";
+
+        driver.findElement(By.cssSelector("a[href= '/schedule']")).click();
+        driver.findElement(By.xpath("//tr[td[contains(text(), 'Year:')]]/td/input")).sendKeys("2025");     //2025
+        driver.findElement(By.xpath("//tr[td[contains(text(), 'Semester:')]]/td/input")).sendKeys("Spring");       //Spring
+        driver.findElement(By.xpath("//button[contains(text(), 'Show Schedule')]")).click();
+        Thread.sleep(SLEEP_DURATION);
+
+        WebElement scheduleTable = driver.findElement(By.xpath("//table[@class='Center' and @border='1']"));
+        List<WebElement> rows = scheduleTable.findElements(By.tagName("tr"));
+        int beforeSize = rows.size();
+
+        WebElement we = driver.findElement(By.cssSelector("a[href= '/addCourse']"));
+        we.click();
+        Thread.sleep(SLEEP_DURATION);
+        WebElement courseRow = driver.findElement(By.xpath("//tr[td[contains(text(), '" + title + "')] and td[contains(text(), '" + section + "')]]"));
+        Thread.sleep(SLEEP_DURATION);
+
+        WebElement enrollButton = courseRow.findElement(By.xpath(".//button[contains(text(), 'Enroll')]"));
+        enrollButton.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        WebElement enrollButton2 = driver.findElement(By.cssSelector("div.MuiDialogActions-root button:nth-of-type(1)"));
+        enrollButton2.click();
+        Thread.sleep(SLEEP_DURATION);
+        WebElement courseRow2 = driver.findElement(By.xpath("//tr[td[contains(text(), '" + title + "')] and td[contains(text(), '" + section + "')]]"));
+        WebElement dropButton = courseRow2.findElement(By.xpath(".//button[contains(text(), 'Drop')]"));
+        assertNotNull(dropButton);
+        Thread.sleep(SLEEP_DURATION);
+
+        driver.findElement(By.cssSelector("a[href= '/schedule']")).click();
+        driver.findElement(By.xpath("//tr[td[contains(text(), 'Year:')]]/td/input")).sendKeys("2025");     //2025
+        driver.findElement(By.xpath("//tr[td[contains(text(), 'Semester:')]]/td/input")).sendKeys("Spring");       //Spring
+        driver.findElement(By.xpath("//button[contains(text(), 'Show Schedule')]")).click();
+        Thread.sleep(SLEEP_DURATION);
+
+        scheduleTable = driver.findElement(By.xpath("//table[@class='Center' and @border='1']"));
+        rows = scheduleTable.findElements(By.tagName("tr"));
+        int afterSize = rows.size();
+        assertEquals(beforeSize + 1, afterSize);
+    }
+
+}
