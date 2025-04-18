ac4b3a2d2c2f97d3e374ccddee04e1a00cd6fc8a
Polina <zhperon@gmail.com>
2025-04-18 12:58:04 -0700

    changed data further and fixed up a controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -43,8 +43,10 @@ insert into enrollment (enrollment_id, grade, section_no, user_id ) values
 (3,  null, 10, 3);
 
 insert into assignment (assignment_id, section_no, title, due_date) values
-( 1, 8, 'db homework 1', '2024-02-01'),
-( 2, 8, 'db homework 2', '2024-02-15');
+( 1, 8, 'db homework 1', '2025-02-01'),
+( 2, 8, 'db homework 2', '2025-02-15'),
+( 3, 8, 'db homework 3', '2025-02-18'),
+( 4, 8, 'db homework 4', '2025-02-19');
 
 insert into grade (grade_id, enrollment_id, assignment_id, score) values
 ( 1, 2, 1, 95);
