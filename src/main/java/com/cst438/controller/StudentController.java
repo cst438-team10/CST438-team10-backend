a4071535317c1bf5e72e41de2df2013fbb189217
Henry Garkanian <hgarkanian@csumb.edu>
2025-04-18 11:07:31 -0700

    Added proper principal calls in StudentController

--- src/main/java/com/cst438/controller/StudentController.java
+++ src/main/java/com/cst438/controller/StudentController.java
@@ -27,6 +27,9 @@ public class StudentController {
     @Autowired
     GradeRepository gradeRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     /**
      students lists there enrollments given year and semester value
      returns list of enrollments, may be empty
@@ -36,10 +39,10 @@ public class StudentController {
    public List<EnrollmentDTO> getSchedule(
            @RequestParam("year") int year,
            @RequestParam("semester") String semester,
-           @RequestParam("studentId") int studentId) {
+           Principal principal) {
        //  hint: use enrollment repository method findByYearAndSemesterOrderByCourseId
-
-       List<Enrollment> enrollments = enrollmentRepository.findByYearAndSemesterOrderByCourseId(year, semester, studentId);
+       User user = userRepository.findByEmail(principal.getName());
+       List<Enrollment> enrollments = enrollmentRepository.findByYearAndSemesterOrderByCourseId(year, semester, user.getId());
        List<EnrollmentDTO> enrollmentDTOs = new ArrayList<>();
 
        for (Enrollment enrollment : enrollments) {
@@ -71,16 +74,15 @@ public class StudentController {
      logged in user must be the student (assignment 7)
      */
     @GetMapping("/assignments")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_STUDENT')")
     public List<AssignmentStudentDTO> getStudentAssignments(
-            @RequestParam("studentId") int studentId,
+            Principal principal,
             @RequestParam("year") int year,
             @RequestParam("semester") String semester) {
 
-        // return a list of assignments and (if they exist) the assignment grade
-        //  for all sections that the student is enrolled for the given year and semester
-        //  hint: use the assignment repository method findByStudentIdAndYearAndSemesterOrderByDueDate
-        List<Assignment> assignments = assignmentRepository.findByStudentIdAndYearAndSemesterOrderByDueDate(studentId, year, semester);
-        List<Enrollment> enrollments = enrollmentRepository.findByYearAndSemesterOrderByCourseId(year, semester, studentId);
+        User user = userRepository.findByEmail(principal.getName());
+        List<Assignment> assignments = assignmentRepository.findByStudentIdAndYearAndSemesterOrderByDueDate(user.getId(), year, semester);
+        List<Enrollment> enrollments = enrollmentRepository.findByYearAndSemesterOrderByCourseId(year, semester, user.getId());
 
         List<AssignmentStudentDTO> assignmentDTOs = new ArrayList<>();
         for (Assignment assignment : assignments) {
